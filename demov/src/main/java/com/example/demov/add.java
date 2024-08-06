b4525c31b0795456c9d0fdf168d424672dba9207
vijaykadpewad <vijaykadpewad01@gmail.com>
2024-08-07 00:09:09 +0530

    added new feature11

--- demov/src/main/java/com/example/demov/add.java
+++ demov/src/main/java/com/example/demov/add.java
@@ -3,7 +3,7 @@ package com.example.demov;
 public class add {
     public static void main(String[] args) {
         System.out.println("demo");
-
+        System.out.println("ss");
         System.out.println();
     }
 }
